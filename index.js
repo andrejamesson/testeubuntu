320b47176bdfe03734b6d464a5a19620e9fc6614
André Jamesson <andre-jamesson@hotmail.com>
2024-05-17 11:44:53 -0300

    app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Porta em que a API vai rodar
+const port = 3200; // Porta em que a API vai rodar
+
+app.use(express.json());
 
 // Rota raiz
 app.get('/', (req, res) => {
@@ -9,14 +11,14 @@ app.get('/', (req, res) => {
 });
 
 app.post('/msg', (req, res) => {
-    const mensagemRecebida = req.body.mensagem; // Extrai a mensagem do corpo da requisição
-  
-    if (mensagemRecebida) {
-      res.status(200).send(`Mensagem recebida: ${mensagemRecebida}`);
-    } else {
-      res.status(400).send('Nenhuma mensagem encontrada no corpo da requisição.');
-    }
-  });
+  const mensagemRecebida = req.body.mensagem; 
+
+  if (mensagemRecebida) {
+    res.status(200).send(`Mensagem recebida: ${mensagemRecebida}`);
+  } else {
+    res.status(400).send('Nenhuma mensagem encontrada no corpo da requisição.');
+  }
+});
 // Inicia o servidor
 app.listen(port, () => {
   console.log(`API rodando em http://localhost:${port}`);
